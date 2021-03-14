0d768bf2437518e87d4872d2675dff339db77dfc
rjampani <jampany.ravindra@gmail.com>
2021-03-14 05:15:18 -0700

    spring boot banner customization demo using configuraton

---
+++ spring-boot-banner-customization-configuration/src/main/java/com/springboot/SampleSpringBootApplication.java
@@ -0,0 +1,10 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+
+public class SampleSpringBootApplication {
+	public static void main(String[] args) {
+		System.out.println("====SampleSpringBootApplication======");
+		SpringApplication.run(SampleSpringBootApplication.class, args);
+	}
+}
---
+++ spring-boot-banner-customization-configuration/src/main/java/com/springboot/SpringBannerCustomizationApplication.java
@@ -0,0 +1,11 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+
+public class SpringBannerCustomizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBannerCustomizationApplication.class, args);
+	}
+
+}
